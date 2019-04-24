9e121918d051023d4cee155fe3abca5ac7b2bbc7
Teodora Tica <ticateodoraioana@gmail.com>
2019-04-24 09:26:57 +0300

    login works

--- app/app.py
+++ app/app.py
@@ -1,13 +1,9 @@
-from copy import deepcopy
 from flask import Flask
 from flask import request
 from typing import List, Dict
 import collections
 import mysql.connector
 import json
-import sys
-import random
-import string
 
 app = Flask(__name__)
 
@@ -62,14 +58,20 @@ def get_books() -> List[Book]:
     return books
 
 
-@app.route('/register')
-def register():
-    username = request.args.get('username', type=str, default="")
-    password = request.args.get('password', type=str, default="")
-    first_name = request.args.get('firstName', type=str, default="")
-    last_name = request.args.get('lastName', type=str, default="")
-    email = request.args.get('email', type=str, default="")
+def add_registration_request(reg_id, username, password, first_name, last_name, email):
+    connection = mysql.connector.connect(**config)
+    cursor = connection.cursor()
+    cursor.execute(
+        f'INSERT INTO registrationRequests VALUES '
+        f'({reg_id}, \'{username}\', \'{password}\', \'{first_name}\', \'{last_name}\', \'{email}\')')
 
+    connection.commit()
+
+    cursor.close()
+    connection.close()
+
+
+def username_exists(username):
     connection = mysql.connector.connect(**config)
     cursor = connection.cursor()
     cursor.execute('SELECT * FROM users')
@@ -81,34 +83,65 @@ def register():
             found = True
             break
 
-    if found:
-        cursor.close()
-        connection.close()
-        return "1-Username already reserved. Please choose another one."
+    cursor.close()
+    connection.close()
+
+    return found
+
+
+def pending_registration(username, email):
+    connection = mysql.connector.connect(**config)
+    cursor = connection.cursor()
 
     cursor.execute('SELECT * FROM registrationRequests')
 
     found = False
-    for (_, un, pw, _, _, _) in cursor:
-        if username == un and password == pw:
+    for (_, un, _, _, _, em) in cursor:
+        if username == un and email == em:
             found = True
             break
 
-    if found:
-        cursor.close()
-        connection.close()
+    cursor.close()
+    connection.close()
+
+    return found
+
+
+def can_login(username, password):
+    connection = mysql.connector.connect(**config)
+    cursor = connection.cursor()
+    cursor.execute('SELECT * FROM users')
+
+    found = False
+    for (_, un, pw, _, _, _) in cursor:
+        if username == un and password == pw:
+            sessions.append(username)
+            found = True
+            break
+
+    cursor.close()
+    connection.close()
+
+    return found
+
+
+@app.route('/register')
+def register():
+    username = request.args.get('username', type=str, default="")
+    password = request.args.get('password', type=str, default="")
+    first_name = request.args.get('firstName', type=str, default="")
+    last_name = request.args.get('lastName', type=str, default="")
+    email = request.args.get('email', type=str, default="")
+
+    if username_exists(username) is True:
+        return "1-Username already reserved. Please choose another one."
+
+    if pending_registration(username, email):
         return "3-There is another pending registration request for this username."
 
     reg_count = get_registration_requests_count()
 
-    cursor.execute(
-        f'INSERT INTO registrationRequests VALUES '
-        f'({reg_count + 1}, \'{username}\', \'{password}\', \'{first_name}\', \'{last_name}\', \'{email}\')')
-
-    cursor.commit()
-
-    cursor.close()
-    connection.close()
+    add_registration_request(reg_count + 1, username, password, first_name, last_name, email)
 
     return "4-Your registration request has been recorded."
 
@@ -121,39 +154,10 @@ def login() -> str:
     if username in sessions:
         return "1-You are already logged in."
 
-    connection = mysql.connector.connect(**config)
-    cursor = connection.cursor()
-    cursor.execute('SELECT * FROM users')
-
-    found = False
-    for (_, un, pw, _, _, _) in cursor:
-        if username == un and password == pw:
-            sessions.append(username)
-            found = True
-            break
-
-    if found:
-        cursor.close()
-        connection.close()
+    if can_login(username, password):
         return "2-Welcome!"
 
-    cursor.execute('SELECT * FROM registrationRequests')
-
-    found = False
-    for (_, un, pw, _, _, _) in cursor:
-        if username == un and password == pw:
-            found = True
-            break
-
-    if found:
-        cursor.close()
-        connection.close()
-        return "3-Your registration request is pending approval."
-
-    cursor.close()
-    connection.close()
-
-    return "4-Invalid username or password. Please try again or register."
+    return "3-Invalid username or password. Please try again or register."
 
 
 @app.route('/logout')
@@ -161,9 +165,10 @@ def logout() -> str:
     username = request.args.get('username', type=str, default="")
 
     if username in sessions:
-        return "You have successfully logged out."
+        sessions.remove(username)
+        return "1-You have successfully logged out."
 
-    return "You were not logged in."
+    return "2-You were not logged in."
 
 
 @app.route('/')
--- client/client.py
+++ client/client.py
@@ -5,6 +5,15 @@ un = ""
 logged_in = False
 
 
+def parse_response(response):
+    tokens = response.split("-")
+
+    if len(tokens) != 2:
+        return -1, "[Internal error] Wrong response format"
+
+    return int(tokens[0], 10), tokens[1]
+
+
 def register(username, password, first_name, last_name, email):
     url = 'http://app:5000/register?username=' + username + \
           '&password=' + password + \
@@ -12,33 +21,57 @@ def register(username, password, first_name, last_name, email):
           '&lastName=' + last_name + \
           '&email=' + email
     request = requests.get(url)
-    print(request.text)
+    code, message = parse_response(request.text)
+    print(message)
+
+    if code != 4:
+        return False
+
+    return True
 
 
 def login(username, password):
     url = 'http://app:5000/login?username=' + username + \
           '&password=' + password
     request = requests.get(url)
+    code, message = parse_response(request.text)
+    print(message)
+
+    if code != 2:
+        return False
+
+    return True
+
+
+def request_suggestion(options):
+    print("TODO")
+
+
+def add_book(title, author, genre):
+    print("TODO")
+
+
+def logout(username):
+    url = 'http://app:5000/logout?username=' + username
+    request = requests.get(url)
     print(request.text)
 
 
-def logout():
-    print("todo")
+def main():
+    global logged_in
+    global un
 
-
-if __name__ == '__main__':
     command_manual = 'Please choose one of the following commands: \n' \
                      '0 - Exit\n' \
                      '1 - Register\n' \
                      '2 - Login\n'
     home_command_manual = 'Please choose one of the following commands: \n' \
-                          '0 - Exit\n' \
+                          '0 - Logout\n' \
                           '1 - Add a book\n' \
                           '2 - Get suggestion\n'
 
+    command = int(input(command_manual))
     while True:
-        command = int(input(command_manual))
-
         if command == 0:
             break
 
@@ -49,16 +82,60 @@ if __name__ == '__main__':
             username = input("Username:")
             password = getpass.getpass("Password:")
 
-            register(username, password, first_name, last_name, email)
+            success = register(username, password, first_name, last_name, email)
+
+            if success is False:
+                print("Please try again!")
 
         elif command == 2:
             username = input("Username:")
             password = getpass.getpass("Password:")
 
-            login(username, password)
+            success = login(username, password)
 
-            if logged_in:
+            if success is True:
+                logged_in = True
+                un = username
                 break
+            else:
+                print("Please try again!")
 
         else:
             print("Incorrect command, please try again.\n")
+
+        command = int(input(command_manual))
+
+    if command == 0:
+        return 0
+
+    while True:
+        command = int(input(home_command_manual))
+
+        if command == 0:
+            if logged_in is True:
+                logout(un)
+                logged_in = False
+            break
+        elif command == 1:
+            title = input("Title:")
+            author = input("Author:")
+            genre = input("Genre:")
+
+            add_book(title, author, genre)
+
+        elif command == 2:
+            options = 'Would you like to select a criteria?\n' \
+                      '0 - None\n' \
+                      '1 - Popularity\n' \
+                      '2 - Author\n' \
+                      '3 - Genre\n'
+            option = input(options)
+
+            request_suggestion(option)
+
+        else:
+            print("Incorrect command, please try again.\n")
+
+
+if __name__ == '__main__':
+    main()
